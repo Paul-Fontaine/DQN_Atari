e94a334fdca5606031a7c5da97b7a0744e5091b1
Paul Fontaine <pfontaine@etu.uqac.ca>
2025-03-04 19:09:04 -0500

    print the device and the number of trainable params

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 from agent import AgentDoubleDQN, device
 from env import get_training_env_and_human_env
 
-print(f"Using {device}")
-
 # Hyperparameters
 EPISODES = 10000
 GAMMA = 0.99
@@ -13,13 +11,17 @@ EPSILON_START = 1.0
 EPSILON_END = 0.1
 EPSILON_DECAY = 1000000
 TARGET_UPDATE = 1000
+CHECKPOINT_FREQUENCY = 100
+RENDERING_FREQUENCY = 2
 ENV_NAME = "ALE/Breakout-v5"
 
 
 def train_agent():
     env, human_env = get_training_env_and_human_env(ENV_NAME)
     agent = AgentDoubleDQN(env, human_env, device, BUFFER_SIZE)
-    agent.train(EPISODES, GAMMA, LEARNING_RATE, BATCH_SIZE, EPSILON_START, EPSILON_END, EPSILON_DECAY, TARGET_UPDATE)
+    print(f"Using {device}")
+    print(f"Number of trainable parameters: {sum(p.numel() for p in agent.dqn.parameters() if p.requires_grad)}")
+    agent.train(EPISODES, GAMMA, LEARNING_RATE, BATCH_SIZE, EPSILON_START, EPSILON_END, EPSILON_DECAY, TARGET_UPDATE, CHECKPOINT_FREQUENCY, RENDERING_FREQUENCY)
     agent.save()
 
 
