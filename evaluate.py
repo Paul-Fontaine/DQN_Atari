58e4ae0e6cff7ffb3b3c7e7bb06c37d1787d041d
Paul Fontaine <pfontaine@etu.uqac.ca>
2025-03-04 19:09:24 -0500

    uses agent methods

--- evaluate.py
+++ evaluate.py
@@ -2,28 +2,10 @@ from agent import AgentDoubleDQN
 from env import env, human_env
 
 
-def evaluate(episodes: int = 10):
+def evaluate():
     agent = AgentDoubleDQN(env, human_env)
     agent.load()
-
-    total_rewards = []
-    for episode in range(episodes):
-        state, _ = env.reset()
-        total_reward = 0
-        done = False
-
-        while not done:
-            action = agent.act(state)
-            next_state, reward, done, _, _ = env.step(action)
-            state = next_state
-            total_reward += reward
-
-        total_rewards.append(total_reward)
-        print(f"Episode {episode}, Total Reward: {total_reward}")
-
-    average_reward = sum(total_rewards) / episodes
-    print(f"Average Reward over 10 episodes: {average_reward}")
-    env.close()
+    agent.evaluate(episodes=10)
 
 
 if __name__ == "__main__":
--- watch.py
+++ watch.py
@@ -6,14 +6,7 @@ def watch():
     agent = AgentDoubleDQN(env, human_env)
     agent.load()
 
-    state, _ = human_env.reset()
-    done = False
-
-    while not done:
-        action = agent.act(state)
-        state, _, done, _, _ = human_env.step(action)
-
-    human_env.close()
+    agent.watch()
 
 
 if __name__ == "__main__":
